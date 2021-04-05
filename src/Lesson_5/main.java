f2c73dbaf8b372e937d6fbf6480ec605bb3b35df
st.nico <157130401678@mail.ru>
2021-04-05 16:33:30 +0300

    Lesson5

--- src/Lesson_5/main.java
+++ src/Lesson_5/main.java
@@ -1,19 +1,27 @@
 package Lesson_5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class main {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args)
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException
     {
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT + 1);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        for (int i = 0; i < cars.length; i++)
+        {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        cb.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        cb.await();
+        cb.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
